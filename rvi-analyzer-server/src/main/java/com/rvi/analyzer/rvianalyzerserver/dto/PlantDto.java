43a2ffeb20ffd939cb341728c9d60b34d8879368
himesh <himesh@cosmoforge.com>
2024-01-09 13:09:05 +0530

    returns null for some properties of customer and plant was fixed.

--- rvi-analyzer-server/src/main/java/com/rvi/analyzer/rvianalyzerserver/dto/PlantDto.java
+++ rvi-analyzer-server/src/main/java/com/rvi/analyzer/rvianalyzerserver/dto/PlantDto.java
@@ -2,13 +2,18 @@ package com.rvi.analyzer.rvianalyzerserver.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
 public class PlantDto {
     private String _id;
     private String name;
+    private String createdBy;
+    private LocalDateTime createdDateTime;
+    private LocalDateTime lastUpdatedDateTime;
     //    private CustomerDto customer;
-//    private String createdBy;
 //    private String status;
 }
