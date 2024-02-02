fdd6c7087cd36224deb5d9f92a34448590d0bb05
himesh <himesh@cosmoforge.com>
2024-02-02 17:34:40 +0530

    production order controller is implemented

---
+++ rvi-analyzer-server/src/main/java/com/rvi/analyzer/rvianalyzerserver/repository/ProductionOrderRepository.java
@@ -0,0 +1,31 @@
+package com.rvi.analyzer.rvianalyzerserver.repository;
+
+import com.rvi.analyzer.rvianalyzerserver.entiy.*;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface ProductionOrderRepository extends ReactiveMongoRepository<ProductionOrder, String> {
+    @Query(
+            value = """
+                    {
+                        "orderId" : {
+                            $eq: ?0
+                        }
+                    }
+                    """
+    )
+    Mono<ProductionOrder> findByOrderId(String orderId);
+
+    @Query(
+            value = """
+                    {
+                        "created-by" : {
+                            $eq: ?0
+                        }
+                    }
+                    """
+    )
+    Flux<ProductionOrder> findByCreatedBy(String createdBy);
+}
